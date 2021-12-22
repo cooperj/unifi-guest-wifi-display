5d3b267628370aaca13849a0e282fb8f33fb5a36
Josh Cooper <hi@joshc.uk>
2021-12-22 23:26:16 +0000

    Added heartbeat <3

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,12 @@ RUN pip3 install requests
 RUN pip3 install Jinja2
 RUN pip3 install python-dotenv
 RUN pip3 install gevent
+RUN pip3 install ping3
 
 COPY . .
 
 EXPOSE 5000
+
+HEALTHCHECK CMD curl --fail http://localhost:5000/heartbeat || exit 1
 
 CMD [ "python", "./app.py" ]
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect, Response
+from requests import status_codes
+from requests.sessions import session
 app = Flask(__name__)
 from gevent.pywsgi import WSGIServer
 from distutils.util import strtobool
+import requests
 
 from Unifi import Unifi
 from Passwords import Passwords
@@ -46,6 +49,40 @@ def regenerate_password():
     else:
         return Response("Password regeneration has not been enabled")
 
+@app.route("/heartbeat")
+def heartbeat():
+    requests.packages.urllib3.disable_warnings()
+    
+    # Check if the controller is accessible
+    get_check = requests.get(os.getenv("UNIFI_CONTROLLER"), verify=False)
+
+    # Can contact controller
+    if get_check.status_code == 200:
+        # Check auth
+        session_token, anti_forgery = Unifi.auth()
+
+        if session_token is not None and anti_forgery is not None:
+            return Response("SERVER_OKAY", 200)
+
+        else:
+            print("Error: Could not auth to Unifi controller")
+            return Response("AUTH_FAILURE", 500)
+        
+    else:
+        print("Error:", get_check.status_code, "cannot talk to the Unifi Controller")
+        return Response("UNIFI_DOWN", 500)
+
+@app.route("/coffee")
+def coffee():
+    return Response("I'M A TEAPOT", 418)
+
+@app.route("/ping")
+def ping():
+    return Response("Pong!", 200)
+
+@app.route("/pingu")
+def pingu():
+    return Response("Noot! Noot!", 202)
 
 if (os.getenv("ENVIRONMENT") == "prod"):
     # Prod
